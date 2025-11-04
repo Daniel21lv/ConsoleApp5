a9592f973a7ffe9bd4d4994f177ee62ed8564cac
Daniel Tahal <24IA25_TAHAL@spsjc.cz>
2025-11-04 08:52:55 +0100

    Přidejte soubory projektu.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace I2A_OOP2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
